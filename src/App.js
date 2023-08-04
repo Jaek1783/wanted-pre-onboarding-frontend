3c7cb8e0cee0730c650b38dd038ad6105c38580b
jaekeun Choi <jaek1783@gmail.com>
2023-08-04 18:10:04 +0900

    회원가입 value값 획득

--- src/App.js
+++ src/App.js
@@ -3,17 +3,49 @@ import {Routes, Route } from 'react-router-dom';
 import SignUpPage from './components/sign-up';
 import SignInPage from './components/sign-in';
 import HomePage from './components/home';
-
+import { useRef,useState} from 'react';
 function App() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const btnRef = useRef(null);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  
+  const checkForm = ()=>{
+        const emailValue = emailRef.current.value;
+        const passwordValue = passwordRef.current.value;
+        if(emailValue.includes('@')){
+          if(passwordValue.length < 8){
+            setIsButtonDisabled(true)
+          }
+          else{
+            setIsButtonDisabled(false)
+          }
+        }
+        else{
+          setIsButtonDisabled(true)
+        }
+      }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      const emailValue = emailRef.current.value.trim();
+      const passwordValue = passwordRef.current.value.trim();
+      console.log(emailValue, passwordValue)
+    }
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpPage 
+                                          emailRef={emailRef} 
+                                          passwordRef={passwordRef} 
+                                          checkForm={checkForm}
+                                          btnRef={btnRef}
+                                          isButtonDisubled={isButtonDisabled}
+                                          handleSubmit={handleSubmit}
+                                          />} />
         <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </div>
   );
 }
-
 export default App;
--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,7 +1,19 @@
 import React from "react";
-const SignUpPage = ()=>{
+const SignUpPage = ({emailRef, passwordRef, btnRef, checkForm, isButtonDisubled, handleSubmit})=>{
     return <div>
         <h1>회원가입</h1>
+        <hr/>
+        <div>
+            <form>
+                <label htmlFor="">
+                    <input type="email" onChange={checkForm} data-testid="email-input" ref={emailRef} required placeholder="@를 꼭 붙여주세요"/>
+                </label>
+                <label htmlFor="">
+                    <input type='password' onChange={checkForm} data-testid="password-input" ref={passwordRef} required placeholder="8자 이상 적어주세요"/>
+                </label>
+                <button data-testid="signup-button" onClick={handleSubmit} ref={btnRef} disabled={isButtonDisubled}>회원가입</button>
+            </form>
+        </div>
     </div>
 };
 export default SignUpPage;
