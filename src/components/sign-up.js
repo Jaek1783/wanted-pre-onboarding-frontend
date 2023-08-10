3d2cfd2ddfda9f06c9f4da61338d263081bf913f
jaekeun Choi <jaek1783@gmail.com>
2023-08-10 10:03:59 +0900

    최종 구현 완료

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -4,10 +4,12 @@ const SignUpPage = ({emailRef, passwordRef, btnRef, checkForm, isButtonDisubled,
     return <div className={styles.container}>
         <h1>회원가입</h1>
             <form>
-                <label htmlFor="">
+                <label>
+                    <span>ID</span>
                     <input type="email" onChange={checkForm} data-testid="email-input" ref={emailRef} required placeholder="@를 꼭 붙여주세요"/>
                 </label>
-                <label htmlFor="">
+                <label>
+                    <span>PW</span>
                     <input type='password' onChange={checkForm} data-testid="password-input" ref={passwordRef} required placeholder="8자 이상 적어주세요"/>
                 </label>
                 <button data-testid="signup-button" onClick={handleSubmit} ref={btnRef} disabled={isButtonDisubled}>회원가입</button>
